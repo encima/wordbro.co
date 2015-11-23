3c4014868239f01993bf8fafc6e69ed9fa20a8dd
encima <encima@gmail.com>
2015-11-23 10:04:26 +0000

    working landing page with submission for bro ideas, need to write tests

--- app/api.py
+++ app/api.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import request, jsonify
 import pymongo
+import random
+import ast
 
 client = pymongo.MongoClient('localhost')
 db = client.bro
@@ -8,11 +10,14 @@ db = client.bro
 @app.route("/api/bro", methods=['GET', 'POST'])
 def get_bro():
     if request.method == 'GET':
-        return 'brobiscuits'
+		if db.bros.find_one({}) is not None:
+			return jsonify({'bro':random.choice(list(db.bros.find()))['bro']})
+		else:
+			return {'bro': 'Bro'}
     elif request.method == 'POST':
-        new_bro = request.json['bro']
-        if not db.bros.find_one({'bro': 'brobeans'}) and 'bro' in new_bro:
-             db.bros.insert({'bro': request.data})
+        new_bro = request.json
+        if db.bros.find_one(new_bro) is None and 'bro' in new_bro['bro']:
+             db.bros.insert(new_bro)
         else:
             return jsonify({'msg': 'No dice, bro'}), 400
-        return jsonify({'msg': 'Nailed it, broski', 'bro': new_bro}), 201
+        return jsonify({'msg': 'Nailed it, broski', 'bro': new_bro['bro']}), 201
--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from app import app
 from app import api
-from flask import render_template
+from flask import render_template, jsonify
+import ast
 
 @app.route('/')
 def index():
   bro = api.get_bro()
-  return render_template('index.html', title='Word, Bro', bro=bro)
+  return render_template('index.html', bro=ast.literal_eval(bro.data)['bro'], title="Wordbro.co")
